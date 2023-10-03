a6e9d2418cd4364502ef06807c587d1a8224a987
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-03 23:18:20 +0200

    Добавлен код

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Himalaya", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.Highlight;
+            label1.Location = new Point(124, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(229, 22);
+            label1.TabIndex = 0;
+            label1.Text = "Резюме Хоменко Виктора";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Verdana", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button1.ForeColor = Color.Gold;
+            button1.Location = new Point(167, 153);
+            button1.Name = "button1";
+            button1.Size = new Size(146, 40);
+            button1.TabIndex = 1;
+            button1.Text = "Открыть";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(484, 261);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Cursor = Cursors.Cross;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,30 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = "Имя: Виктор Хоименко\r\nТелефон: +380686700001\r\nEmail: viktorplus3@gmail.com";
+            string title = "Контактная информация";
+            int len = message.Length;
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            message = "Специальность: Инженер энергетик\r\nУчебное заведение: Запорожская Государственная Инженерная Аккадемия\r\nГод окончания: 2001\r\nДополнительное образование:\r\nСпециальность: Бухгалтерский учет и аудит\r\nУчебное заведение: Запорожская Государственная Инженерная Аккадемия\r\nГод окончания: 2001";
+            title = "Образование";
+            len += message.Length;
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            message = "Продвинутое владение языками программирования С++ и С#.\r\nЗнание основ алгоритмов и структур данных.\r\nЗнание основных принципов разработки по Agile-методологии.\r\nЗнание английского языка на техническом уровне.\r\nЦель: Получить позицию программиста для применения знаний инженерной энергетики, бухгалтерского учета и аудита в сфере разработки программного обеспечения на C++ и C#.";
+            title = "Технические навыки";
+            len += message.Length;
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            message = "Создание программного продукта для учета и анализа энергопотребления.\r\nРазработка приложения для автоматизации бухгалтерского учета и аудита.";
+            title = "Примерные проекты";
+            len += message.Length;
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            message = "Рекомендации: По запросу";
+            len += message.Length;
+            title = $"Среднее количество символов {(int)len/5} Общее {len}";
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
     }
 }
