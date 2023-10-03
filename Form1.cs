21253f2b8050eeecb4d14ba95f59182414f6f20c
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-03 21:51:51 +0200

    MessageBox ?? MainForm mainform = new MainForm();

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,25 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string title = "Notification";
+            string message = "Congratulation!";
+            var result = MessageBox.Show(message, title, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation);
+            if (result == DialogResult.Yes)
+            {
+                Form mainform = new Form();
+                mainform.Show();
+            }
+            if (result == DialogResult.No)
+            {
+                this.Close();
+            }
+            if (result == DialogResult.Cancel)
+            {
+                MessageBox.Show(message, title, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation);
+            }
+        }
     }
 }
