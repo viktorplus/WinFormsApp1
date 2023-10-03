742c78de34a71ee245fe2f91a72e97c842629b81
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-03 22:06:39 +0200

    Альтернативный вариант кода

--- Form1.cs
+++ Form1.cs
@@ -14,12 +14,17 @@ namespace WinFormsApp1
 
         private void button2_MouseMove(object sender, MouseEventArgs e)
         {
-            button2.Top -= e.Y;
-            button2.Left += e.X;
-            if(button2.Top < -10 || button2.Top > 100)
-                button2.Top = 60;
-            if (button2.Left < -80 || button2.Left > 250)
-                button2.Left = 120;
+            //button2.Top -= e.Y;
+            //button2.Left += e.X;
+            //if(button2.Top < -10 || button2.Top > 100)
+            //    button2.Top = 60;
+            //if (button2.Left < -80 || button2.Left > 250)
+            //    button2.Left = 120;
+            Random random = new Random();
+            int x = random.Next(0, this.Width-button2.Width);
+            int y = random.Next(0, this.Height - button2.Height);
+            button2.Location = new Point(x, y);
+            button2.Update();
         }
     }
 }
