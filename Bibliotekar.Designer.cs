1282cdbaa6d124b016e671cac022745da2f36c5e
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-09 10:00:27 +0200

    на уроек

--- Bibliotekar.Designer.cs
+++ Bibliotekar.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsApp1
 {
-    partial class Form1
+    partial class Bibliotekar
     {
         /// <summary>
         /// Required designer variable.
--- Login.cs
+++ Login.cs
@@ -1,10 +1,37 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
 namespace WinFormsApp1
 {
     public partial class Login : Form
     {
+        string username = "admin";
+        string password = "admin";
         public Login()
         {
             InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tb_login.Text == username && password == tb_password.Text)
+            {
+                this.Visible = false;
+                MessageBox.Show("Вітаємо, " + username + "!", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                HomeUser homeForm = new HomeUser(this);
+                var result = homeForm.ShowDialog();
+                if (DialogResult.Cancel == result)
+                {
+                    this.Visible = true;
+                }
+                else
+                {
+                    MessageBox.Show("Помилка відкриття форми!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Невірний логін або пароль!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
 
