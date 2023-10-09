ea3673b88bcd97584af1e525f8b9ff0e4cb9a0eb
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-10 00:48:36 +0200

    test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(365, 115);
+            label1.Name = "label1";
+            label1.Size = new Size(169, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Поймай меня если сможешь";
+            label1.MouseMove += Label1_MouseMove;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,39 @@
 namespace WinFormsApp1
 {
+    /*Задание 5
+Разработать приложение «убегающий статик»:) Суть приложения:
+на форме расположен статический элемент управления («статик»).
+Пользователь пытается подвести курсор мыши к «статику», и, если
+курсор находиться близко со статиком, элемент управления «убегает». Предусмотреть перемещение «статика» только в пределах формы. 
+    */
     public partial class Form1 : Form
     {
+        private Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+            //label1.MouseMove += Label1_MouseMove;
+        }
+
+        private void Label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int threshold = 50; // Пороговое расстояние для перемещения Label
+
+            // Рассчитываем расстояние между текущим положением Label и курсора мыши
+            int deltaX = e.X - label1.Left - label1.Width / 2;
+            int deltaY = e.Y - label1.Top - label1.Height / 2;
+            double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+            // Если расстояние меньше порогового значения, перемещаем Label
+            if (distance < threshold)
+            {
+                // Новые случайные координаты для Label в пределах формы
+                int newX = random.Next(0, this.ClientSize.Width - label1.Width);
+                int newY = random.Next(0, this.ClientSize.Height - label1.Height);
+
+                label1.Location = new Point(newX, newY);
+            }
         }
     }
 }
