5b8aaa36a1cd5b31846dbc51ebbe5b64b94ff063
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-10 02:13:38 +0200

    финал

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(149, 51);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(176, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(149, 115);
+            button1.Name = "button1";
+            button1.Size = new Size(176, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Определить день недели";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(149, 185);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(176, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(149, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(244, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Введите дату в формате: День / Месяц/ Год";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(149, 167);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Результат";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(445, 278);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,34 @@
+using System.Globalization;
+
 namespace WinFormsApp1
 {
+    /*Задание 6
+Написать программу, которая по введенной дате определяет день
+недели. Результат выводить в текстовое поле (желательно по-русски). 
+     */
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string userInput = textBox1.Text;
+
+            // Попытка преобразования введенной строки в дату
+            if (DateTime.TryParse(userInput, out DateTime selectedDate))
+            {
+                // Определение дня недели на русском языке
+                string dayOfWeek = selectedDate.ToString("dddd", new CultureInfo("ru-RU"));
+
+                // Вывод результата в текстовое поле
+                textBox2.Text = $"День недели: {dayOfWeek}";
+            }
+            else
+            {
+                MessageBox.Show("Некорректный формат даты!", "Ошибка");
+            }
         }
     }
 }
