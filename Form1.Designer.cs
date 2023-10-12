76efd0d08a40756e24ce3187bf0e50a07186e51a
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-12 23:59:10 +0200

    финиш

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(354, 114);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 0;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Enabled = false;
+            dateTimePicker2.Location = new Point(354, 163);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 1;
+            dateTimePicker2.ValueChanged += dateTimePicker_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(240, 249);
+            label1.Name = "label1";
+            label1.Size = new Size(156, 54);
+            label1.TabIndex = 2;
+            label1.Text = "0 дней ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(240, 122);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Начальная дата";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(240, 163);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Конечная дата";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(240, 209);
+            label4.Name = "label4";
+            label4.Size = new Size(182, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Количество дней между датами";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Name = "Form1";
+            Text = "Расчет времени между датами";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,32 @@
+using System.Windows.Forms;
+
 namespace WinFormsApp1
 {
+    /* 
+     * Задание 2
+Подсчитайте количество дней между выбранными датами с помощью DateTimePicker и выведите результат на форму с использование
+элемента Label. Форму главного окна сделайте в виде круга.
+    */
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime startdata = dateTimePicker1.Value;
+            DateTime enddata = dateTimePicker2.Value;
+            TimeSpan timeSpan = enddata - startdata;
+            int daysDifference = (int)timeSpan.TotalDays;
+
+            // Отображаем количество дней на Label
+            label1.Text = "Количество дней: " + daysDifference.ToString();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker2.Enabled = true;
         }
     }
 }
