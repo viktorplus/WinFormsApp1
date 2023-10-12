c1021ea497c57b3a94c1cb7f9fe55e030784a359
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-12 15:07:52 +0200

    point 3

--- Form1.cs
+++ Form1.cs
@@ -2,13 +2,19 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        Guds guds;
         public Form1()
         {
             InitializeComponent();
+            guds = new Guds();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            guds.gud.Add(new Gud ("Хот Дог", 0, 4));
+            guds.gud.Add(new Gud("Гамбургер", 0, 5.4));
+            guds.gud.Add(new Gud("Картошка Фри", 0, 7.2));
+            guds.gud.Add(new Gud("Кока Кола", 0, 4.4));
 
         }
 
@@ -17,18 +23,33 @@ namespace WinFormsApp1
             if (cb_hotdog.Checked)
             {
                 tb_hotdog_count.Enabled = true;
-                tb_hotdog_count.Text = "1";
             }
             else
             {
                 tb_hotdog_count.Enabled = false;
-                tb_hotdog_count.Text = "0";
             }
         }
 
         private void tb_hotdog_count_changed(object sender, EventArgs e)
         {
-            if()
+            if (int.TryParse(tb_hotdog_count.Text, out int targetNumber)) // проверка на введенное число int
+            {
+                if (targetNumber > 0)
+                {
+                    guds.gud[0].SetCount(targetNumber);
+                    tb_hotdog_price.Enabled = true;
+                    tb_hotdog_price.Text = guds.gud[0].Summa.ToString();
+                }
+                else
+                {
+                    tb_hotdog_price.Enabled = false;
+                }
+            }
+            else
+            {
+                tb_hotdog_price.Enabled = false;
+
+            }
         }
     }
 }
---
+++ Guds.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class Gud
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+        public double Summa { get; set; }
+        public Gud() {
+            Name = string.Empty;
+            Count = 0;
+            Price = 0;
+            Summa = 0;
+        }
+        public Gud (string name, int coumt, double price)
+        {
+            Name = name;
+            Count = coumt;  
+            Price = price;
+            if (Count > 0)
+            {
+                Summa = Price * Count;
+            }
+            else
+            {
+                Summa = 0;
+            }
+        }
+        public void SetCount(int count)
+        {
+            Count = count;
+            if (Count > 0)
+            {
+                Summa = Price * count;
+            }
+            else
+            {
+                Summa = 0;
+            }
+        }
+    }
+    public class Guds
+    {
+       public List<Gud> gud = new List<Gud>(); 
+    }
+}
