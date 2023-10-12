62720f2b26b499e6522a1ef98291b04d28315ec2
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-12 23:12:27 +0200

    финиш

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            lastname = new TextBox();
+            button1 = new Button();
+            name = new TextBox();
+            label2 = new Label();
+            vatername = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            sex = new ComboBox();
+            datebirhday = new DateTimePicker();
+            label5 = new Label();
+            familystatus = new ComboBox();
+            label6 = new Label();
+            label7 = new Label();
+            info = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Фамилия";
+            // 
+            // lastname
+            // 
+            lastname.Location = new Point(233, 24);
+            lastname.Name = "lastname";
+            lastname.Size = new Size(199, 23);
+            lastname.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(233, 312);
+            button1.Name = "button1";
+            button1.Size = new Size(199, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Сохранить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // name
+            // 
+            name.Location = new Point(233, 53);
+            name.Name = "name";
+            name.Size = new Size(199, 23);
+            name.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Имя";
+            // 
+            // vatername
+            // 
+            vatername.Location = new Point(233, 82);
+            vatername.Name = "vatername";
+            vatername.Size = new Size(199, 23);
+            vatername.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(27, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Отчество";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(27, 119);
+            label4.Name = "label4";
+            label4.Size = new Size(30, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Пол";
+            // 
+            // sex
+            // 
+            sex.FormattingEnabled = true;
+            sex.Location = new Point(233, 111);
+            sex.Name = "sex";
+            sex.Size = new Size(199, 23);
+            sex.TabIndex = 8;
+            // 
+            // datebirhday
+            // 
+            datebirhday.Location = new Point(232, 140);
+            datebirhday.Name = "datebirhday";
+            datebirhday.Size = new Size(200, 23);
+            datebirhday.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(27, 148);
+            label5.Name = "label5";
+            label5.Size = new Size(90, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Дата рождения";
+            // 
+            // familystatus
+            // 
+            familystatus.FormattingEnabled = true;
+            familystatus.Location = new Point(233, 169);
+            familystatus.Name = "familystatus";
+            familystatus.Size = new Size(199, 23);
+            familystatus.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(27, 177);
+            label6.Name = "label6";
+            label6.Size = new Size(103, 15);
+            label6.TabIndex = 11;
+            label6.Text = "Семейный статус";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(27, 230);
+            label7.Name = "label7";
+            label7.Size = new Size(175, 15);
+            label7.TabIndex = 14;
+            label7.Text = "Дополнительная информация";
+            // 
+            // info
+            // 
+            info.Location = new Point(233, 207);
+            info.Multiline = true;
+            info.Name = "info";
+            info.Size = new Size(199, 84);
+            info.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 361);
+            Controls.Add(info);
+            Controls.Add(label7);
+            Controls.Add(familystatus);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(datebirhday);
+            Controls.Add(sex);
+            Controls.Add(label4);
+            Controls.Add(vatername);
+            Controls.Add(label3);
+            Controls.Add(name);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(lastname);
+            Controls.Add(label1);
+            MaximumSize = new Size(720, 480);
+            Name = "Form1";
+            Text = "Анкета в файл";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox lastname;
+        private Button button1;
+        private TextBox name;
+        private Label label2;
+        private TextBox vatername;
+        private Label label3;
+        private Label label4;
+        private ComboBox sex;
+        private DateTimePicker datebirhday;
+        private Label label5;
+        private ComboBox familystatus;
+        private Label label6;
+        private Label label7;
+        private TextBox info;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -5,6 +9,77 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
+            // Создаем список вариантов семейного статуса
+            List<string> familyStatusOptions = new List<string>
+            {
+                "Холост/Не замужем",
+                "Замужем/Женат",
+                "Разведен/Разведена",
+                "Вдовец/Вдова",
+                "Другое"
+            };
+
+            // Привязываем список к ComboBox для семейного статуса
+            familystatus.DataSource = familyStatusOptions;
+
+            // Создаем список вариантов пола
+            List<string> genderOptions = new List<string> { "Мужской", "Женский", "Диверс" };
+
+            // Привязываем список к ComboBox для пола
+            sex.DataSource = genderOptions;
+
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Получаем значения из элементов управления
+            string lastnameValue = lastname.Text;
+            string nameValue = name.Text;
+            string vaternameValue = vatername.Text;
+            string sexValue = sex.SelectedItem?.ToString();
+            string dateOfBirthValue = datebirhday.Value.ToShortDateString();
+            string familyStatusValue = familystatus.SelectedItem?.ToString();
+            string additionalInfoValue = info.Text;
+
+            // Проверка на пустые поля
+            if (string.IsNullOrWhiteSpace(lastnameValue) || string.IsNullOrWhiteSpace(nameValue))
+            {
+                MessageBox.Show("Заполните обязательные поля: Фамилия и Имя");
+                return;
+            }
+
+            // Формируем строку для сохранения
+            string dataToSave = $"{lastnameValue}, {nameValue}, {vaternameValue}, {sexValue}, {dateOfBirthValue}, {familyStatusValue}, {additionalInfoValue}";
+
+            // Сохраняем данные в файл
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("data.txt", true)) // путь к файлу
+                {
+                    sw.WriteLine(dataToSave);
+                }
+
+                MessageBox.Show("Информация успешно сохранена в файле.");
+                ResetForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка при сохранении данных: {ex.Message}");
+            }
+        }
+
+        private void ResetForm()
+        {
+            // Сбросить значения элементов управления
+            lastname.Clear();
+            name.Clear();
+            vatername.Clear();
+            sex.SelectedIndex = -1;
+            datebirhday.Value = DateTime.Now;
+            familystatus.SelectedIndex = -1;
+            info.Clear();
+        }
+
+
     }
 }
