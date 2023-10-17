20e4b76a8de0d5638598ec1a0a17bce3fb08bff2
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-17 12:09:56 +0200

    сделана форма

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtName = new TextBox();
+            txtSurname = new TextBox();
+            txtEmail = new TextBox();
+            txtPhone = new TextBox();
+            btnAdd = new Button();
+            btnDelete = new Button();
+            btnEdit = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            btnExportText = new Button();
+            btnImportText = new Button();
+            btnExportXML = new Button();
+            btnImportXML = new Button();
+            lstUsers = new ListBox();
+            SuspendLayout();
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(165, 45);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(163, 23);
+            txtName.TabIndex = 0;
+            // 
+            // txtSurname
+            // 
+            txtSurname.Location = new Point(165, 87);
+            txtSurname.Name = "txtSurname";
+            txtSurname.Size = new Size(163, 23);
+            txtSurname.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(165, 128);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(163, 23);
+            txtEmail.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            txtPhone.Location = new Point(165, 172);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(163, 23);
+            txtPhone.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(40, 225);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(97, 23);
+            btnAdd.TabIndex = 4;
+            btnAdd.Text = "Добавить";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(253, 225);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 23);
+            btnDelete.TabIndex = 5;
+            btnDelete.Text = "Удалить";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(143, 225);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(104, 23);
+            btnEdit.TabIndex = 6;
+            btnEdit.Text = "Редактировать";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(40, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(40, 136);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 15);
+            label3.TabIndex = 9;
+            label3.Text = "e-mail";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(40, 180);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Телефон";
+            // 
+            // btnExportText
+            // 
+            btnExportText.Location = new Point(40, 291);
+            btnExportText.Name = "btnExportText";
+            btnExportText.Size = new Size(150, 38);
+            btnExportText.TabIndex = 11;
+            btnExportText.Text = "Экспорт в текстовый файл";
+            btnExportText.UseVisualStyleBackColor = true;
+            // 
+            // btnImportText
+            // 
+            btnImportText.Location = new Point(196, 291);
+            btnImportText.Name = "btnImportText";
+            btnImportText.Size = new Size(151, 38);
+            btnImportText.TabIndex = 12;
+            btnImportText.Text = "Импорт из текстового файла";
+            btnImportText.UseVisualStyleBackColor = true;
+            // 
+            // btnExportXML
+            // 
+            btnExportXML.Location = new Point(40, 355);
+            btnExportXML.Name = "btnExportXML";
+            btnExportXML.Size = new Size(150, 23);
+            btnExportXML.TabIndex = 13;
+            btnExportXML.Text = "Экспорт в XML файл";
+            btnExportXML.UseVisualStyleBackColor = true;
+            // 
+            // btnImportXML
+            // 
+            btnImportXML.Location = new Point(196, 355);
+            btnImportXML.Name = "btnImportXML";
+            btnImportXML.Size = new Size(151, 23);
+            btnImportXML.TabIndex = 14;
+            btnImportXML.Text = "Импорт из XML файла";
+            btnImportXML.UseVisualStyleBackColor = true;
+            // 
+            // lstUsers
+            // 
+            lstUsers.FormattingEnabled = true;
+            lstUsers.ItemHeight = 15;
+            lstUsers.Location = new Point(374, 44);
+            lstUsers.Name = "lstUsers";
+            lstUsers.Size = new Size(397, 334);
+            lstUsers.TabIndex = 15;
+            lstUsers.SelectedIndexChanged += lstUsers_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstUsers);
+            Controls.Add(btnImportXML);
+            Controls.Add(btnExportXML);
+            Controls.Add(btnImportText);
+            Controls.Add(btnExportText);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnEdit);
+            Controls.Add(btnDelete);
+            Controls.Add(btnAdd);
+            Controls.Add(txtPhone);
+            Controls.Add(txtEmail);
+            Controls.Add(txtSurname);
+            Controls.Add(txtName);
+            Name = "Form1";
+            Text = "Контакты";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtName;
+        private TextBox txtSurname;
+        private TextBox txtEmail;
+        private TextBox txtPhone;
+        private Button btnAdd;
+        private Button btnDelete;
+        private Button btnEdit;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button btnExportText;
+        private Button btnImportText;
+        private Button btnExportXML;
+        private Button btnImportXML;
+        private ListBox lstUsers;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,14 @@
-namespace WinFormsApp1
+﻿namespace WinFormsApp1
+/*Задание 2
+Написать приложение – анкету, которую предлагается заполнить
+пользователю: имя, фамилия, e-mail, телефон. После нажатия на кнопку «Добавить» Информация о пользователе попадает в ListBox, в котором храниться информация о всех пользователях. Также, в ListBox,
+по клику на строку с информацией о пользователе следует предусмотреть возможность удаления пользователя из коллекции элементов ListBox, а также редактирования. Редактирование информации о
+пользователе производиться путем подстановки данных из ListBox, в
+соответствующие поля для ввода новой информации.
+Предусмотреть:
+■ экспорт/импорт всей информации о пользователях в текстовый файл;
+■ экспорт/импорт всей информации о пользователях в *.xml
+файл.*/
 {
     public partial class Form1 : Form
     {
@@ -6,5 +16,71 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string surname = txtSurname.Text;
+            string email = txtEmail.Text;
+            string phone = txtPhone.Text;
+
+            string userInfo = $"{name}, {surname}, {email}, {phone}";
+
+            lstUsers.Items.Add(userInfo);
+
+            // Очистите поля TextBox после добавления информации.
+            txtName.Clear();
+            txtSurname.Clear();
+            txtEmail.Clear();
+            txtPhone.Clear();
+        }
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (lstUsers.SelectedIndex != -1)
+            {
+                string name = txtName.Text;
+                string surname = txtSurname.Text;
+                string email = txtEmail.Text;
+                string phone = txtPhone.Text;
+
+                string editedInfo = $"{name}, {surname}, {email}, {phone}";
+
+                // Получаем индекс выбранного элемента.
+                int selectedIndex = lstUsers.SelectedIndex;
+
+                // Удаляем выбранный элемент.
+                lstUsers.Items.RemoveAt(selectedIndex);
+
+                // Добавляем отредактированную информацию на том же индексе.
+                lstUsers.Items.Insert(selectedIndex, editedInfo);
+            }
+        }
+
+        private void lstUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstUsers.SelectedIndex != -1)
+            {
+                string selectedUserInfo = lstUsers.SelectedItem.ToString();
+                string[] userInfoParts = selectedUserInfo.Split(new char[] { ',' }, StringSplitOptions.TrimEntries);
+
+                if (userInfoParts.Length == 4)
+                {
+                    txtName.Text = userInfoParts[0].Trim();
+                    txtSurname.Text = userInfoParts[1].Trim();
+                    txtEmail.Text = userInfoParts[2].Trim();
+                    txtPhone.Text = userInfoParts[3].Trim();
+                }
+            }
+        }
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstUsers.SelectedIndex != -1)
+            {
+                lstUsers.Items.RemoveAt(lstUsers.SelectedIndex);
+            }
+        }
+
     }
 }
