321f36d44055955d4ba5a5464e9ca36ad5f69162
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-18 01:36:48 +0200

    point3

--- Class1.cs
+++ Class1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static WinFormsApp1.Users;
 
 namespace WinFormsApp1
 {
@@ -10,31 +11,42 @@ namespace WinFormsApp1
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string Adress { get; set; }
+        public string Address { get; set; }
         public string Telefon { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public DateTime DateReg { get; }
         public string Status { get; set; }
+        public List<Book> BooksReading { get; set; } // Список книг, которые пользователь читает
 
-        public User(string name, string surname, string adress, string telefon, string login, string password)
+        public User(string name, string surname, string address, string telefon, string login, string password)
         {
             Name = name;
             Surname = surname;
-            Adress = adress;
+            Address = address;
             Telefon = telefon;
             Login = login;
             Password = password;
             DateReg = DateTime.Now;
             Status = "user";
+            BooksReading = new List<Book>();
         }
 
         public override string ToString()
         {
-            return $"{Name} : {Surname} : {Adress} : {Telefon} ";
+            return $"{Name} : {Surname} : {Address} : {Telefon}";
+        }
+
+        public void ReadBook(Book book)
+        {
+            BooksReading.Add(book);
+        }
+
+        public void ReturnBook(Book book)
+        {
+            BooksReading.Remove(book);
         }
     }
-
     public class Users
     {
         public static List<User> users = new List<User>();
@@ -53,7 +65,52 @@ namespace WinFormsApp1
 
             return matchingUser; // Возвращает объект User, если пользователь с таким логином и паролем найден, или null, если не найден
         }
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public int Year { get; set; }
+            public string Genre { get; set; }
+            public string ISBN { get; set; }
 
+            public Book(string title, string author, int year, string genre, string isbn)
+            {
+                Title = title;
+                Author = author;
+                Year = year;
+                Genre = genre;
+                ISBN = isbn;
+            }
+        }
+
+        public class Library
+        {
+            public static List<Book> books = new List<Book>();
+
+            public static void AddBook(Book book)
+            {
+                books.Add(book);
+            }
+
+            public static void RemoveBook(Book book)
+            {
+                books.Remove(book);
+            }
+
+            public static List<Book> SearchBooksByTitle(string title)
+            {
+                return books.Where(book => book.Title.Contains(title, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            public static List<Book> SearchBooksByAuthor(string author)
+            {
+                return books.Where(book => book.Author.Contains(author, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            public static List<Book> SearchBooksByGenre(string genre)
+            {
+                return books.Where(book => book.Genre.Contains(genre, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+        }
     }
-
 }
